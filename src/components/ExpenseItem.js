28307d03b28260ee4de6a7f7e388e4a3f1c3f1cd
Sukanta01 <72244847+Sukanta01@users.noreply.github.com>
2023-03-20 19:08:15 +0530

    expenses added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ExpenseItem() {
+  return (
+    <div>
+        <h2>Expense items</h2>
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+
+
+
+
+        </div>
+  )
+}
+
+export default ExpenseItem
