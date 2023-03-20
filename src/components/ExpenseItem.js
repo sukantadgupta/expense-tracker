086507c1b9990d79d2d32f0374437aa201025cfd
Sukanta01 <72244847+Sukanta01@users.noreply.github.com>
2023-03-20 20:56:13 +0530

    expense place added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import './ExpenseItem.css'
+
+
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+    const locationOfExpenditure = "Siliguri"
   return (
-    <div>
-        <h2>Expense items</h2>
-        <div>Food Rs 10</div>
-        <div>Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
+    <div className = "expense-item">
+      <div>{expenseDate.toISOString()} </div>
+      <div className='expense-item__description'> 
+      <h2>{expenseTitle}</h2>
+      <div className='expense-item__place'>{locationOfExpenditure}</div>
+      
+      <div className='expense-item__price'>${expenseAmount} </div>
+      </div>
 
 
 
